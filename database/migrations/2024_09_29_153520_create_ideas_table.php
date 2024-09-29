b17b2bf20b239e0935cf6afa80278f34f7efd5c9
Enrico Giorgi <enrico.giorgi92@gmail.com>
2024-09-29 18:48:51 +0200

    migrations tabella ideas

---
+++ database/migrations/2024_09_29_153520_create_ideas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     * int id
+     * 255 varchar
+     * unsigned int default 0
+     * created at - updated at
+     */
+    public function up(): void
+    {
+        Schema::create('ideas', function (Blueprint $table) {
+            $table->id();
+            $table->string('idea');
+            $table->unsignedInteger('likes')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ideas');
+    }
+};
