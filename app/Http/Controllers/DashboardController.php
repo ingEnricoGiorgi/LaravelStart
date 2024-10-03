d957172eb55dbd4f40e63c7364bc6647c4aa2b8f
Enrico Giorgi <enrico.giorgi92@gmail.com>
2024-10-03 18:02:07 +0200

    show data from db table ideas

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,25 +7,31 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
- public function index(){
-      $idea = new Idea();
-      $idea->idea = 'test';
-      $idea->likes = 0;
-      $idea->save();
-      return view('dashboard');
-     /*
-          $users = [
-              [
-                  'nome' => 'luca',
-                  'eta' => 21
-              ],
-              [
-                  'nome' => 'lucat',
-                  'eta' => 22
-              ]
-          ];
-          return view('users.profile',['usersList' =>$users]);
-          */
-      }
+    public function index()
+    {
+        /*   $idea = new Idea();
+           $idea->idea = 'test';
+           $idea->likes = 0;
+           $idea->save();
+           return view('dashboard');
+        */
+        /*
+             $users = [
+                 [
+                     'nome' => 'luca',
+                     'eta' => 21
+                 ],
+                 [
+                     'nome' => 'lucat',
+                     'eta' => 22
+                 ]
+             ];
+             return view('users.profile',['usersList' =>$users]);
+             */
+
+        $ideas = Idea::all();
+
+        return view('dashboard', compact('ideas'));
+    }
 
 }
