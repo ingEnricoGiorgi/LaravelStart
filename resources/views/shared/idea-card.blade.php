ee619794db06c89eb7970b1779e82e27f5d59ab4
Enrico Giorgi <enrico.giorgi92@gmail.com>
2024-10-11 20:23:53 +0200

    modificato style

--- resources/views/shared/idea-card.blade.php
+++ resources/views/shared/idea-card.blade.php
@@ -2,6 +2,12 @@
     <div class="px-3 pt-4 pb-2">
         <div class="d-flex align-items-center justify-content-between">
             <div class="d-flex align-items-center">
+                <img style="width:50px" class="me-2 avatar-sm rounded-circle"
+                     src="https://api.dicebear.com/6.x/fun-emoji/svg?seed=Mario" alt="Mario Avatar">
+                <div>
+                    <h5 class="card-title mb-0"><a href="#"> Mario
+                        </a></h5>
+                </div>
             </div>
             <div>
                 <form method="POST" action="{{route('ideas.destroy', $idea->id)}}">
@@ -10,12 +16,7 @@
                     <button class="btn btn-danger btn-sm">X</button>
                 </form>
             </div>
-          <!--  <img style="width:50px" class="me-2 avatar-sm rounded-circle"
-                 src="https://api.dicebear.com/6.x/fun-emoji/svg?seed=Mario" alt="Mario Avatar">
-            <div>
-                <h5 class="card-title mb-0"><a href="#"> Mario
-                    </a></h5>
-            </div>-->
+
         </div>
     </div>
     <div class="card-body">
