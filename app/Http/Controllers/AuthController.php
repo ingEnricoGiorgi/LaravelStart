8ae6c678660672f852385e6554cd04e8677d110b
Enrico Giorgi <enrico.giorgi92@gmail.com>
2024-10-18 19:06:06 +0200

    login - register- logout logic

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,24 +8,58 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function register(){
+    public function register()
+    {
 
         return view('auth.register');
     }
 
-    public function store(Request $request){
-      $validated= $request->validate([
-          'name'=>'required|min:3|max:40',
-          'email'=>'required|email|unique:users,email',
-          'password'=>'required|confirmed|min:6'
-      ]);
-      $user = User::create(
-          [
-              'name'=>$validated['name'],
-              'email'=>$validated['email'],
-              'password'=>Hash::make($validated['password'])
-          ]
-      );
-      return redirect('/')->with('success', 'Account created successfull');
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|min:3|max:40',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        $user = User::create(
+            [
+                'name' => $validated['name'],
+                'email' => $validated['email'],
+                'password' => Hash::make($validated['password'])
+            ]
+        );
+        return redirect('/')->with('success', 'Account created successfull');
+    }
+
+    public function login()
+    {
+        return view('auth.login');
+    }
+
+    public function authenticate(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+
+        if (auth()->attempt($validated)) {
+            request()->session()->regenerate();
+            return redirect('/')->with('success', 'login successfull');
+
+        }
+
+        return redirect('/login')->withErrors(
+            ['email' => 'These credentials do not match our records.']
+        );
+
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+        request()->session()->invalidate();
+        request()->session()->regenerate();
+        return redirect('/')->with('success', 'You have been logged out');
     }
 }
