773071b4b52b39edde580de99e41becf651e2e1f
Enrico Giorgi <enrico.giorgi92@gmail.com>
2024-10-20 16:31:02 +0200

    vite is working!

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -12,16 +12,18 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
           integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
           crossorigin="anonymous" referrerpolicy="no-referrer" />
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
+<h1>CIAO</h1>
 @include('layout.nav')
 <div class="container py-4">
-    {{-- content goes here --}}
-    @yield('content')
+{{-- content goes here --}}
+@yield('content')
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
+    integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
 </script>
 </body>
 
