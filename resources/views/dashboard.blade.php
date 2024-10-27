7a06fddf22d096d9cd8b33187880bb82ba20c2f2
Enrico Giorgi <enrico.giorgi92@gmail.com>
2024-10-27 15:45:36 +0100

    fix query ricerca mantenuta, fix no results found, fix mantengo ricerca nella search bar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,16 +8,23 @@
             @include('shared.success-message')
             {{--@include('shared.error-message')--}}
             @include('shared.submit-idea')
+            @if($ideas->isEmpty())
+                <h2 class="text-center">NO RESULTS FOUND</h2>
+
+            @else
             @foreach($ideas as $idea)
                 <div class="mt-3">
                     @include('shared.idea-card')
                 </div>
             @endforeach
+            <!-- PAGINAZIONE -->
             <div class="mt-3">
-                {{$ideas->links()}}
+                {{$ideas->withQueryString()->links()}}
             </div>
+            @endif
 
         </div>
+
         <div class="col-3">
             @include('shared.search-bar')
             @include('shared.follow-box')
--- resources/views/shared/search-bar.blade.php
+++ resources/views/shared/search-bar.blade.php
@@ -6,7 +6,7 @@
 
         <form action="{{ route('dashboard') }}" method="GET"><!--  guarda nel file web.php-->
             @csrf
-        <input name='search' placeholder="..." class="form-control w-100" type="text">
+        <input value="{{request('search','')}}" name='search' placeholder="..." class="form-control w-100" type="text">
         <button class="btn btn-dark mt-2"> Search</button>
         </form>
     </div>
